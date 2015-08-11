75377dc722e909c2df3048b8b3a3e42e3f31147d
Paul <paul@optimised.today>
2015-08-11 16:13:35 +0100

    Initial navigation (contact panel still outstanding)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :global_site_settings
+  before_action :global_site_settings, :load_objects
+
+  def index
+  end
 
   private
 
+  def load_objects
+    @header_menu = Optimadmin::Menu.new(name: "header")
+  end
+  
   def global_site_settings
     @global_site_settings ||= Optimadmin::SiteSetting.current_environment
   end
